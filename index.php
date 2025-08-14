667bb19c743f18105b3cc95642edfceb18120fd2
chalistarano <chalistarano@gmail.com>
2025-08-14 22:58:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
 
-function is_bot() {
-    $user_agent = $_SERVER['HTTP_USER_AGENT'];
-    $bot_agents = ['Googlebot', 'bingbot', 'AhrefsBot', 'Google-Site-Verification', 'Google-InspectionTool', 'TelegramBot'];
-    
-    foreach ($bot_agents as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
-        }
-    }
-    
-    return false;
-}
-
-
-if (is_bot()) {
-
-    echo file_get_contents('masukin.txt');
-    exit;
-}
-
-
-?><?php
-
   header('location:home.php'); 
 
 ?>
